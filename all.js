7cc2958738594ce41ff097f751a6510bcbb8f5ac
steveycee <stevey.cee@gmail.com>
2022-03-24 19:20:36 +0000

    feat: first pass

---
+++ all.js
@@ -0,0 +1,80 @@
+const { performance } = require("perf_hooks");
+
+let shuffleArray = (array) => {
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+};
+
+let generate = (n) => {
+  console.log("STARTING!");
+  let array = [];
+  let start = performance.now();
+  for (let i = 0; i < n; i++) {
+    const domains = ["com", "co.uk", "fr", "net", "org", "edu", "gov", "kyoto"];
+    const providers = ["outlook", "gmail", "aol", "bt", "facebook"];
+    const chars = "abcdefghijklmnopqrstuvwxyz1234567890";
+    let string = "";
+    for (var ii = 0; ii < 15; ii++) {
+      string += chars[Math.floor(Math.random() * chars.length)];
+    }
+    // All variable sections randomised
+    // let email = string + "@" + string + ".com";
+    // More life like emails by choose from a rebuilt list of common domains and providers
+    let email =
+      string +
+      "@" +
+      providers[Math.floor(Math.random() * providers.length)] +
+      "." +
+      domains[Math.floor(Math.random() * domains.length)];
+
+    array.push(email);
+  }
+  console.log(array);
+  let end = performance.now();
+  console.log(
+    "Tried generating",
+    n,
+    "emails. Managed to generate",
+    array.length,
+    "emails generated in",
+    end - start,
+    "milliseconds"
+  );
+  //Doubling the array to make sure I've got 50% more
+  const doubledArray = array.concat(array);
+  console.log("Doubled the array to: ", doubledArray);
+  // return doubledArray;
+
+  // SHUFFLE
+  let shuffledArray = shuffleArray(doubledArray);
+  console.log("Shuffled array resulting in: ", shuffledArray);
+  // SORT
+  let start1 = performance.now();
+  let product = [...new Set(shuffledArray)];
+  console.log("Removed duplicates resulting in: ", product);
+  // console.log([...new Set(shuffledArray)]);
+  // return [...new Set(emailsArray)]
+  let end1 = performance.now();
+  console.log("The operation occured on: ", Object.keys(shuffledArray).length) +
+    " items.";
+  console.log("The operation took: ", end1 - start1, " milliseconds");
+};
+
+generate(5);
+
+// sort = (emails) => {
+//   let start = performance.now();
+//   console.log([...new Set(emails)]);
+//   // return [...new Set(emailsArray)]
+//   let end = performance.now();
+//   console.log("The operation occured on: ", Object.keys(emails).length),
+//     " items.";
+//   console.log("The operation took: ", end - start, " milliseconds");
+// };
+
+// sort(generate);
